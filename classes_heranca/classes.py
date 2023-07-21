1ce749299b89c42b9739ce1bc9b3b66aad61380d
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-07-21 09:50:34 +0100

    classes v1

--- classes_heranca/classes.py
+++ classes_heranca/classes.py
@@ -1,17 +1,27 @@
+class Ser_vivo:
+     def respirar(self):
+          print("a respirar")
 
-class Pessoa:
+
+class Pessoa(Ser_vivo):
      def __init__(self, nome: str, idade: int, email: str):
           self.nome = nome
           self.idade = idade
           self.email = email
+
+     @staticmethod
+     def teste():
+          print("metodo estatico")
 
      def __str__(self):
           return f"Pessoa(nome:{self.nome}, idade:{self.idade}, email: {self.email})"
 
 class Aluno(Pessoa):
      def __init__(self, nome: str, idade: int, email: str, turma:str):
-          Pessoa.__init__(nome, idade, email)
+          super().__init__(nome, idade, email)
+
           self.turma = turma
 
      def __str__(self):
-          return f"{Pessoa.__str__()}, turma: {self.turma}"
+          Pessoa.teste()
+          return f"{super().__str__()}, turma: {self.turma}"
---
+++ classes_heranca/main.py
@@ -0,0 +1,7 @@
+from classes import Aluno
+
+
+al1 = Aluno("João", 17, "jaoa@gmail.com", "turma 1")
+
+print(al1)
+al1.respirar()
