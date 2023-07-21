3adc5d67f3dbb192522c8e046f25b467f4cbe800
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-07-21 11:35:23 +0100

    fastAPI for

--- fastAPI_demo1/main.py
+++ fastAPI_demo1/main.py
@@ -13,6 +13,10 @@ async def root():
     return {"message": "Hello World teste"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/hello/{name}/{ano}")
+async def say_hello(name: str, ano: int):
+    msg = {"message": f"Hello {name}", "ano": ano}
+    for i in range(10):
+        msg[f"Mensagem {i}"] = f"conteudo da msg 1: {i}"
+
+    return msg
