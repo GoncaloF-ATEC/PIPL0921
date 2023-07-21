d0a986d906a3e71300e3646bb225f5aaf16578fb
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-07-21 11:50:29 +0100

    fastAPI for em lista de obj

---
+++ fastAPI_demo1/Pessoa.py
@@ -0,0 +1,8 @@
+
+class Pessoa:
+    def __init__(self, nome: str, idade: int):
+        self.nome = nome
+        self.idade = idade
+
+
+ListaPessoas = [Pessoa("Joao", 30), Pessoa("Maria", 38)]
--- fastAPI_demo1/main.py
+++ fastAPI_demo1/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from  Pessoa import ListaPessoas
 
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -9,14 +11,26 @@ async def root():
 
 
 @app.get("/teste")
-async def root():
-    return {"message": "Hello World teste"}
+async def rootTeste():
+    p = ListaPessoas[0]
+    return {"nome": f"{p.nome}", "idade": p.idade }
 
+@app.get("/pessoas")
+async def rootTeste():
+    msg = []
+    for p in ListaPessoas:
+        msg.append({"nome": f"{p.nome}", "idade": p.idade })
 
+    return msg
+
+#          /hello/Goncalo/2023
 @app.get("/hello/{name}/{ano}")
 async def say_hello(name: str, ano: int):
     msg = {"message": f"Hello {name}", "ano": ano}
     for i in range(10):
-        msg[f"Mensagem {i}"] = f"conteudo da msg 1: {i}"
+        msg[f"Mensagem {i}"] = f"conteudo da msg {i}: {i}"
 
     return msg
+
+
+
